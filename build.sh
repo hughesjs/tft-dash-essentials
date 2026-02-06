3e55c52dd9fc892009dbe04a1a6f32a123375567
James Hughes <james@pyrosoftsolutions.co.uk>
2026-02-06 23:20:13 +0000

    Update build.sh to detect platform and build correctly

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
-g++ -o testsdl testsdl.cpp -I/Library/Frameworks/SDL2.framework/Headers -F/Library/Frameworks -framework SDL2
+#!/bin/bash
+# Build script for TFT Dash Display
+# Automatically detects platform (macOS vs Linux)
+
+set -e
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS build
+    echo "Building for macOS..."
+    g++ -o testsdl testsdl.cpp -I/Library/Frameworks/SDL2.framework/Headers -F/Library/Frameworks -framework SDL2
+else
+    # Linux build (Raspberry Pi)
+    echo "Building for Linux..."
+    g++ -o testsdl testsdl.cpp -lSDL2 -lpthread
+fi
+
+echo "Build complete: testsdl"
